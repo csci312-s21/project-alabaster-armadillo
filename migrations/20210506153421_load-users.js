e0d764bc02ce93d24f5b9b9aadec63f41ed24636
Replit user <replituser@example.com>
2021-05-10 18:36:24 +0000

    passes

--- migrations/20210506153421_load-users.js
+++ migrations/20210506153421_load-users.js
@@ -1,6 +1,6 @@
 exports.up = function (knex) {
   return knex.schema.createTable("panthers", (table) => {
-    table.foreign("id").references("users.id").onDelete("CASCADE");
+    table.increments("id");
     table.string("firstName").notNullable();
     table.string("lastName").notNullable();
     table.string("email").unique().notNullable();
