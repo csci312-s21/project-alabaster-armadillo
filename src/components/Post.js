0497c3bcc1b44c61ce3370799044c4840dfdf557
Replit user <replituser@example.com>
2021-05-23 19:12:08 +0000

    merged styles

--- src/components/Post.js
+++ src/components/Post.js
@@ -12,42 +12,32 @@ import { useState, useEffect } from "react";
 import Box from "@material-ui/core/Box";
 import Toolbar from "@material-ui/core/Toolbar";
 import Grid from "@material-ui/core/Grid";
-
 //import Typography from '@material-ui/core/Typography';
 import React from "react";
 //import { spacing } from "@material-ui/system";
 import LikeButton from "../components/LikeButton";
 import ReportButton from "../components/ReportButton";
-
-
 export default function Post({ user }) {
-
   const [liked, setLike] = useState("unlike");
   const [reported, setReport] = useState("unreported");
-
   const [counter, setCounter] = useState(8);
-
   const handleClick = (action) => {
     if(action !== liked){
       setLike(action);
-     
     }
   }
   const handleClickReport = (action) => {
     if(action !== reported){
-      setReport(action);  
-    }   
+      setReport(action);
+    }
   }
-
   let isReported;
     if(reported === "reported"){
     isReported = true;
-   
   }
   else{
     isReported = false;
   }
-
  let isLiked;
   if(liked === "like"){
     isLiked = true;
@@ -55,14 +45,11 @@ export default function Post({ user }) {
   else{
     isLiked = false;
   }
-  
   useEffect(() => {
     const timer = counter > 0 && setInterval(() => setCounter(counter - 1), 1000);
     return () => clearInterval(timer);
   }, [counter]);
-
     return (
-  
       <Box
         justifyContent="center"
         alignItems="center"
@@ -77,48 +64,26 @@ export default function Post({ user }) {
         hyphens = "manual"
         overflow = "hidden"
       >
-    
-        <h3 className = {styles.userName}> { post.user } </h3>
-        <p className = {styles.postText}> { post.contents }</p>
-        
+        <h3 className = {styles.userName}> {user.firstName} { user.lastName} </h3>
+        <p className = {styles.postText}> { user.post }</p>
+        <p className = {styles.postText}> { user.postTime }</p>
         <Toolbar>
           <Grid justify="space-between" container>
             <Grid item>
               <LikeButton liked = {isLiked} handleClick = {handleClick}> </LikeButton>
               <ReportButton reported = {isReported} handleClick = {handleClickReport}> </ReportButton>
             </Grid>
-
-
-    <div className = {styles.post} >
-      <h2> { user.firstName } { user.lastName } </h2>
-      <p> { user.post }</p>
-      
-      <p className = {styles.timestamp}>{user.postTime}</p>
-      
-      <p> {counter}</p>
-
-      <span className = {styles.like} onClick={() => setLikes(likes)} type="button">♥</span>
-    </div>
-
-    );
-
             <Grid item >
               <p className = {styles.counter}> {counter}</p>
             </Grid>
           </Grid>
         </Toolbar>
       </Box>
-    
   );
-
 }
-
 Post.propTypes = {
-  user: PropTypes.object.isRequired
+  user: PropTypes.object.isRequired,
 };
-
-//<small> { counter }</small>
-
 
 
 
